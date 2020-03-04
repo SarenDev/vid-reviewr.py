1f620262db2436001cd32e4e755d7ec4c70b6ffd
Saren Dev <54124554+SarenDev@users.noreply.github.com>
2020-03-04 19:04:22 +0300

    Early spring cleaning
    A good dusting to reduce line count

--- script/features/syscall.py
+++ script/features/syscall.py
@@ -5,15 +5,13 @@ from .texts import texter
 
 def syscall(cause):
     if platform.system()=="Linux":
-        if cause=="pause":
-            subprocess.run("read -n 1 -r -s -p 'Press any key to continue...'", shell=True, executable="/bin/bash")
-        if cause=="cls":
-            subprocess.run("clear", shell=True, executable="/bin/bash")
+        if cause=="pause": subprocess.run("read -n 1 -r -s -p 'Press any key to continue...'", shell=True, executable="/bin/bash")
+        if cause=="cls": subprocess.run("clear", shell=True, executable="/bin/bash")
+    
     elif platform.system()=="Windows":
-        if cause=="pause":
-            subprocess.run("pause", shell=True)
-        if cause=="cls":
-            subprocess.run("cls", shell=True)
+        if cause=="pause": subprocess.run("pause", shell=True)
+        if cause=="cls": subprocess.run("cls", shell=True)
+    
     else:
         print(texter("incomp"))
         sleep(10)
